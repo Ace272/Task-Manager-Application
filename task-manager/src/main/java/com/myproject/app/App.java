e407a2520a2fc61fc5797aa86469208bf78ae4cf
Ace272 <ace272@icloud.com>
2023-10-03 19:11:19 -0400

    Create Task Class and add appropriate attributes

--- task-manager/src/main/java/com/myproject/app/App.java
+++ task-manager/src/main/java/com/myproject/app/App.java
@@ -4,6 +4,12 @@ package com.myproject.app;
  * Hello world!
  *
  */
+
+public class Task{
+    private String name;
+    private String description;
+    private Bool status;
+}
 public class App 
 {
     public static void main( String[] args )
