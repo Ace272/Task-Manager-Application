c0107ed28a6aa9bcffec9e45c8a7c388e0a1672a
Ace272 <ace272@icloud.com>
2023-10-24 05:34:05 -0400

    Set up db connection
    Pom.xml set up and a new package added for db. Plus dbconnection class added and configured

---
+++ task-manager/src/main/java/com/myproject/db/DatabaseConnection.java
@@ -0,0 +1,43 @@
+package com.myproject.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:derby:./myTasksdb;create=false";
+    private static final String DB_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    private Connection connection;
+
+    public DatabaseConnection() {
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public boolean isConnected() {
+        try {
+            return connection != null && !connection.isClosed();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void close() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
