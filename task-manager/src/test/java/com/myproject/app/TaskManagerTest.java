0623173421073ed66f4399eb1ac15532b0a58311
Ace272 <ace272@icloud.com>
2023-11-01 21:13:23 -0400

    Implement Mockito in task manager test file
    Unfortunatly it keep on crushing needs fixing

--- task-manager/src/test/java/com/myproject/app/TaskManagerTest.java
+++ task-manager/src/test/java/com/myproject/app/TaskManagerTest.java
@@ -1,34 +1,39 @@
-package com.myproject.app;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
-import java.io.File;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class TaskManagerTest {
     private TaskManager taskManager;
 
+    @Mock
+    private TaskGroup taskGroup;
+
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         taskManager = new TaskManager();
     }
 
-    // @Test
-    // public void testObjectToXml() {
-    //     // Create a sample TaskGroup and specify the name and description
-    //     TaskGroup tg = new TaskGroup();
-    //     String name = "Sample Task Group";
-    //     String desc = "A sample task group";
+    @Test
+    public void testObjectToXml() {
+        // Create a sample TaskGroup and specify the name and description
+        String name = "Sample Task Group";
+        String desc = "A sample task group";
 
-    //     // Define the expected file path
-    //     String expectedFilePath = "src/main/resources/Task-Group/Sample Task Group.xml";
+        // Define the expected file path
+        String expectedFilePath = "src/main/resources/Task-Group/Sample Task Group.xml";
 
-    //     // Call the objectToXml method
-    //     taskManager.objectToXml(tg, name, desc);
+        // Mock behavior of the taskGroup object
+        Mockito.when(taskGroup.getName()).thenReturn(name);
+        Mockito.when(taskGroup.getDescription()).thenReturn(desc);
 
-    //     // Check if the file exists in the expected path
-    //     File file = new File(expectedFilePath);
-    //     assertTrue(file.exists());
-    // }
+        // Call the objectToXml method
+        taskManager.objectToXml(taskGroup, name, desc);
+
+        // Check if the file exists in the expected path
+        File file = new File(expectedFilePath);
+        assertTrue(file.exists());
+    }
 }
