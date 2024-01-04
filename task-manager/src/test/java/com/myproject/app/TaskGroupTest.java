98fdab4a9047f0c8ff490e677932fcff9b3b3cac
abdums <ace272@icloud.com>
2024-01-04 10:56:49 -0500

    First test for TaskGroupTest successfully done

--- task-manager/src/test/java/com/myproject/app/TaskGroupTest.java
+++ task-manager/src/test/java/com/myproject/app/TaskGroupTest.java
@@ -3,7 +3,17 @@ package com.myproject.app;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class TaskGroupTest {
+    TaskItem ti1 = new TaskItem("joe", "For the doe", TaskItem.Status.IN_PROGRESS);
+    TaskItem ti2 = new TaskItem("Location", "For the opportunity", TaskItem.Status.DONE);
+    TaskItem ti3 = new TaskItem("Row", "Square it in", TaskItem.Status.TODO);
 
     @Test
-    public void createTaskGroupObj(){};
+    public void createTaskGroupObj(){
+        TaskGroup tg1 = new TaskGroup();
+        tg1.setName("Hello");
+        tg1.setDescription("First one");
+
+        assertEquals("Hello", tg1.getName());
+        assertEquals("First one", tg1.getDescription());
+    };
 }
