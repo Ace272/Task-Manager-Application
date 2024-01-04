f0741f65b3ae4552c83ed4e5828103a08d4fbb11
abdums <ace272@icloud.com>
2024-01-04 11:47:33 -0500

    TaskGrouptTEst completed test for all functionalities

--- task-manager/src/test/java/com/myproject/app/TaskGroupTest.java
+++ task-manager/src/test/java/com/myproject/app/TaskGroupTest.java
@@ -16,4 +16,35 @@ public class TaskGroupTest {
         assertEquals("Hello", tg1.getName());
         assertEquals("First one", tg1.getDescription());
     };
+
+    @Test
+    public void testGetTaskList(){
+        //create a task group obj
+        TaskGroup tgl = new TaskGroup();
+        tgl.setName("yolo");
+        tgl.setDescription("This and that");
+
+        //Add task items to the task list
+        tgl.addToTL(ti1);
+        tgl.addToTL(ti2);
+
+        //test if these task items have been added
+        assertTrue(tgl.getTaskList().contains(ti1));
+        assertTrue(tgl.getTaskList().contains(ti2));
+    };
+
+    @Test
+    public void testViewTasklist(){
+        //create a task group obj
+        TaskGroup tvl = new TaskGroup();
+        tvl.setName("holo");
+        tvl.setDescription("to and fro");
+
+        //Add task items to the task list
+        tvl.addToTL(ti1);
+        tvl.addToTL(ti3);
+
+        // Use view task list to view it on console
+        tvl.viewTaskList();
+    }
 }
