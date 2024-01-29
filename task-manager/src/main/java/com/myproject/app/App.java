87498e215651720a59c862bdccd76e9a98ce63f6
abdums <ace272@icloud.com>
2024-01-29 17:36:07 -0500

    Reverting to a9ddf6b

--- task-manager/src/main/java/com/myproject/app/App.java
+++ task-manager/src/main/java/com/myproject/app/App.java
@@ -2,10 +2,13 @@ package com.myproject.app;
 
 public class App 
 {
-    public void start(){
-        // Interactive ClI goes here
-        CommandLineInterface cli = new CommandLineInterface();
-        cli.go();
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //Creating a task object
+        TaskItem task1 = new TaskItem("This project", "Good intro practice", TaskItem.Status.IN_PROGRESS);
+        //getting task status
+        System.out.println("Status: " + task1.getStatus());
     }
 
 }
--- task-manager/src/main/java/com/myproject/app/CommandLineInterface.java
+++ task-manager/src/main/java/com/myproject/app/CommandLineInterface.java
@@ -2,11 +2,14 @@ package com.myproject.app;
 
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CommandLineInterface {
 
     private Scanner scanner;
     private String username;
+    private static final Logger logger = LogManager.getLogger(CommandLineInterface.class);
 
     public CommandLineInterface() {
         this.scanner = new Scanner(System.in);
@@ -17,6 +20,7 @@ public class CommandLineInterface {
         System.out.println("Welcome to the Task Manager app");
         System.out.println("What is your name?");
         username = scanner.nextLine();
+        logger.info("Name of user received: "+ username);
         System.out.println("Hello, " + username);
         try {
             TimeUnit.SECONDS.sleep(10);
